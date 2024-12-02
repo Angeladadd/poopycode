18a5b1a4d56dbe642eca824b920f07c5db8587c2
Angeladaddy <sjtusunchenge@gmail.com>
2024-12-02 13:19:25 +0000

    add button, the callback function doesn't work

--- sidebar/PoopySideView.js
+++ sidebar/PoopySideView.js
@@ -1,27 +1,26 @@
+const fs = require('fs');
+const path = require('path');
 class PoopySideView {
-    constructor(context) {
-      this.context = context;
-    }
-  
-    resolveWebviewView(webviewView) {
-      console.log('Resolving Webview for poopyCodeView');
-  
-      webviewView.webview.options = {
-        enableScripts: true,
-      };
-  
-      webviewView.webview.html = `
-        <!DOCTYPE html>
-        <html>
-        <head>
-          <title>Poopy Code</title>
-        </head>
-        <body>
-          <p>💩 Did you poop today?</p>
-        </body>
-        </html>
-      `;
-    }
+  constructor(context) {
+    this.context = context;
   }
-  
-  module.exports = PoopySideView;
+
+  resolveWebviewView(webviewView) {
+    console.log('Resolving Webview for poopyCodeView');
+
+    webviewView.webview.options = {
+      enableScripts: true,
+    };
+
+    // Resolve the path to the HTML file
+    const htmlPath = path.join(this.context.extensionPath, 'resources', 'sidebar.html');
+
+    // Read the HTML file content
+    const htmlContent = fs.readFileSync(htmlPath, 'utf8');
+
+    // Set the HTML content in the Webview
+    webviewView.webview.html = htmlContent;
+  }
+}
+
+module.exports = PoopySideView;
